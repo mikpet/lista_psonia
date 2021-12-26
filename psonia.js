2a3241f4f975957b1e3ce1050ed87e7dcc85b87c
mikpet <93352357+mikpet@users.noreply.github.com>
2021-12-26 15:57:34 +0200

    Είναι η λίστα για τα ψώνια

---
+++ psonia.js
@@ -0,0 +1,32 @@
+const addButton = document.querySelector("#add-button");
+addButton.addEventListener("click", addItem);
+
+const addedItem = document.querySelector('#item');
+const shoppingList = document.querySelector('#shopping-list')
+
+addedItem.addEventListener("keyup", (e)=> {
+    console.log(e.code);
+    if (e.code === "Enter"){addItem();}
+})
+
+function addItem(){
+    console.log(addedItem.value);
+    let newItem = addedItem.value;
+    if (newItem.length>0) {
+        let item = document.createElement('li');
+        item.style.width = "100%";
+        item.style.maxWidth = "20em";
+        item.style.fontSize = "1.2em";
+        item.textContent = newItem;
+        let itemButton = document.createElement('button');
+        itemButton.textContent = 'διαγραφή'
+        itemButton.addEventListener("click", deleteItem)
+        item.appendChild(itemButton);
+        shoppingList.appendChild(item);
+        addedItem.value="";
+    }
+}
+
+function deleteItem(){
+    this.parentNode.remove();
+}
